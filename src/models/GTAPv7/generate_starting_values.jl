4e3e0c0668988750b971a696586365b4f3133e1b
mivanicERS <67010332+mivanicERS@users.noreply.github.com>
2024-12-16 09:30:02 -0500

    Change symbols to strings

--- src/models/GTAPv7/generate_starting_values.jl
+++ src/models/GTAPv7/generate_starting_values.jl
@@ -14,17 +14,17 @@ function generate_starting_values(; hSets, hData, hParameters)
 
     # Prepare a set of fixed parameters
     fixed = Dict(
-        k => NamedArray(trues(size(data[k])), names(data[k])) for (k) in [:to, :tfe, :tx, :txs, :tm, :tms, :tfd, :tfm, :tpd, :tpm, :tgd, :tgm, :tid, :tim, :tinc, :qesf, :qe, :ppa]
+        k => NamedArray(trues(size(data[k])), names(data[k])) for (k) in ["to", "tfe", "tx", "txs", "tm", "tms", "tfd", "tfm", "tpd", "tpm", "tgd", "tgm", "tid", "tim", "tinc", "qesf", "qe", "ppa"]
     )
 
-    fixed[:ppa][:, :] .= false
+    fixed["ppa"][:, :] .= false
     ## The price of the first commodity in the first region is fixed
-    fixed[:ppa][comm[1], reg[1]] = true
+    fixed["ppa"][comm[1], reg[1]] = true
 
     # Calculate calibrated_parameter initial values
 
-    calibrated_parameters = Dict(String(k) => parameters[k] for k ∈ [:α_pca, :α_qca, :α_qes2, :α_qfa, :α_qfdqfm, :α_qfe, :α_qga, :α_qgdqgm, :α_qia, :α_qidqim, :α_qintva, :α_qinv, :α_qpdqpm, :α_qst, :α_qtmfsd, :α_qxs, :β_qpa, :γ_pca, :γ_qca, :γ_qes2, :γ_qfa, :γ_qfdqfm, :γ_qfe, :γ_qga, :γ_qgdqgm, :γ_qia, :γ_qidqim, :γ_qintva, :γ_qpdqpm, :γ_qst, :γ_qxs, :σyg, :σyp, :δ])
+    calibrated_parameters = Dict(String(k) => parameters[k] for k ∈ ["α_pca", "α_qca", "α_qes2", "α_qfa", "α_qfdqfm", "α_qfe", "α_qga", "α_qgdqgm", "α_qia", "α_qidqim", "α_qintva", "α_qinv", "α_qpdqpm", "α_qst", "α_qtmfsd", "α_qxs", "β_qpa", "γ_pca", "γ_qca", "γ_qes2", "γ_qfa", "γ_qfdqfm", "γ_qfe", "γ_qga", "γ_qgdqgm", "γ_qia", "γ_qidqim", "γ_qintva", "γ_qpdqpm", "γ_qst", "γ_qxs", "σyg", "σyp", "δ"])
 
-    return (sets=sets, parameters=parameters, data=data, calibrated_parameters=calibrated_parameters, fixed=Dict(String(k) => fixed[k] for k ∈ keys(fixed)))
+    return (sets=sets, parameters=parameters, data=data, calibrated_parameters=calibrated_parameters, fixed=fixed)
 
 end
