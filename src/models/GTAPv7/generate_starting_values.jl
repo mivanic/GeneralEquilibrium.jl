6821723914bbb4afd6ca2e4d2f8ab09e44bfd260
mivanicERS <67010332+mivanicERS@users.noreply.github.com>
2024-12-18 11:49:04 -0500

    Added rho to exported calibrated parameters

--- src/models/GTAPv7/generate_starting_values.jl
+++ src/models/GTAPv7/generate_starting_values.jl
@@ -24,7 +24,7 @@ function generate_starting_values(; hSets, hData, hParameters)
 
     # Calculate calibrated_parameter initial values
 
-    calibrated_parameters = Dict(String(k) => parameters[k] for k ∈ ["α_pca", "α_qca", "α_qes2", "α_qfa", "α_qfdqfm", "α_qfe", "α_qga", "α_qgdqgm", "α_qia", "α_qidqim", "α_qintva", "α_qinv", "α_qpdqpm", "α_qst", "α_qtmfsd", "α_qxs", "β_qpa", "γ_pca", "γ_qca", "γ_qes2", "γ_qfa", "γ_qfdqfm", "γ_qfe", "γ_qga", "γ_qgdqgm", "γ_qia", "γ_qidqim", "γ_qintva", "γ_qpdqpm", "γ_qst", "γ_qxs", "σyg", "σyp", "δ"])
+    calibrated_parameters = Dict(String(k) => parameters[k] for k ∈ ["α_pca", "α_qca", "α_qes2", "α_qfa", "α_qfdqfm", "α_qfe", "α_qga", "α_qgdqgm", "α_qia", "α_qidqim", "α_qintva", "α_qinv", "α_qpdqpm", "α_qst", "α_qtmfsd", "α_qxs", "β_qpa", "γ_pca", "γ_qca", "γ_qes2", "γ_qfa", "γ_qfdqfm", "γ_qfe", "γ_qga", "γ_qgdqgm", "γ_qia", "γ_qidqim", "γ_qintva", "γ_qpdqpm", "γ_qst", "γ_qxs", "σyg", "σyp", "δ", "ρ"])
 
     return (sets=sets, parameters=parameters, data=data, calibrated_parameters=calibrated_parameters, fixed=fixed)
 
